abc3a57a74e9522246392cbe08b2c33fa1b7a9c5
Zhenya <ifoundluck@gmail.com>
2021-11-20 00:13:05 +0200

    Add files via upload

---
+++ synonyms.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+
+using namespace std;
+
+
+int main() {
+
+	map<string, set<string>> mp;
+
+	int q = 0;
+	cin >> q;
+
+	for (int i = 0; i < q; i++)
+	{
+		string operation;
+		cin >> operation;
+
+		if (operation == "ADD")
+		{
+			string word1, word2;
+			cin >> word1 >> word2;
+
+			if (word1 != word2)
+			{
+				mp[word1].insert(word2);
+				mp[word2].insert(word1);
+			}
+		}
+
+		if (operation == "COUNT")
+		{
+			string word;
+			cin >> word;
+
+			cout << mp[word].size() << endl;
+		}
+
+		if (operation == "CHECK")
+		{
+			string word1, word2;
+			cin >> word1 >> word2;
+
+			if (word1 != word2)
+			{
+				if (mp[word1].count(word2) == 1 || mp[word2].count(word1) == 1)
+					cout << "YES" << endl;
+				else
+					cout << "NO" << endl;
+			}
+		}
+	}
+
+}
